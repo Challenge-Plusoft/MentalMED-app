47761d6d35f32a485cc31ccc56b1b68787c6cc45
Vinícius Yoda <vyoda4604@gmail.com>
2023-05-03 11:37:48 -0300

    Orientation
    instalando react-native-orientation-locker para bloquear que tela vire para horizontal

--- MentalMED/App.js
+++ MentalMED/App.js
@@ -1,7 +1,14 @@
 import Rotas from './src/routes/rotas';
 import { TemaProvider } from "./src/contexts/TemaContext";
+import Orientation from 'react-native-orientation-locker';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(() => {
+    Orientation.lockToPortrait(); // bloqueia a orientação da tela em retrato
+  }, []);
+
   return (
     <TemaProvider>
       <Rotas />
